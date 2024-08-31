07b0a5ab84ce68c386ffddaed3bf8d6ad3dc52ac
aminul-islam-sajid <85724932+aminul-islam-sajid@users.noreply.github.com>
2024-08-31 22:07:38 +0600

    iconify install

--- src/shared/AllNav/Nav1.jsx
+++ src/shared/AllNav/Nav1.jsx
@@ -1,8 +1,15 @@
+import { Icon } from '@iconify-icon/react/dist/iconify.js';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Nav1 = () => {
     return (
         <div>
+            <div>
+                <Link>
+                    <Icon icon="logos:youtube-icon" width="64" height="64" />
+                </Link>
+            </div>
             <h1>1st nav</h1>
         </div>
     );
