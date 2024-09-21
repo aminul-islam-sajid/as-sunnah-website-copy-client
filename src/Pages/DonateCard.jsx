063c5608b7bc98610526fca1d48ea57fd7b7d1d9
aminul-islam-sajid <85724932+aminul-islam-sajid@users.noreply.github.com>
2024-09-21 22:36:34 +0600

    card

---
+++ src/Pages/DonateCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CardPrimary from '../components/reUse/CardPrimary';
+
+const DonateCard = () => {
+    return (
+        <div>
+            <CardPrimary />
+        </div>
+    );
+};
+
+export default DonateCard;
