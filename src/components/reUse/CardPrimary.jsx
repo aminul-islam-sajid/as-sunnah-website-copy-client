bcb2cab0ab1521021579f5aa7afad72291105488
aminul-islam-sajid <85724932+aminul-islam-sajid@users.noreply.github.com>
2024-09-22 07:24:47 +0600

    ***

--- src/components/reUse/CardPrimary.jsx
+++ src/components/reUse/CardPrimary.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 
 const CardPrimary = ({ img, title, subTitle, btn }) => {
     return (
-        <div className='rounded-md w-[333px] h-[506px] text-center'>
-            <img src={img} alt="iamge" />
-            <div className='h-4/6'>
+        <div className='rounded-md w-[333px] h-[506px] text-center border-2 flex flex-col justify-between'>
+            <div>
+                <img className='h-3/5 w-full ' src={img} alt="image" />
                 <h1>{title}</h1>
                 <p>{subTitle}</p>
-                <button>{btn}</button>
             </div>
+            <div className='p-4 '>
+                <button className='bg-primaryColor w-full py-2 mt-auto rounded-md text-white'>{btn}</button>
+
+            </div>
+
         </div>
     );
 };
 
 export default CardPrimary;
+
