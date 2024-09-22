bc0cbbf350b7c090abcc5f296b59b98665e4f537
aminul-islam-sajid <85724932+aminul-islam-sajid@users.noreply.github.com>
2024-09-22 07:53:11 +0600

    card modify

--- src/components/reUse/CardPrimary.jsx
+++ src/components/reUse/CardPrimary.jsx
@@ -2,20 +2,17 @@ import React from 'react';
 
 const CardPrimary = ({ img, title, subTitle, btn }) => {
     return (
-        <div className='rounded-md w-[333px] h-[506px] text-center border-2 flex flex-col justify-between'>
-            <div>
-                <img className='h-3/5 w-full ' src={img} alt="image" />
-                <h1>{title}</h1>
-                <p>{subTitle}</p>
+        <div className='rounded-lg w-[333px] h-[506px] shadow-md text-center bg-white flex flex-col justify-between'>
+            <div className=''>
+                <img className='h-3/5 w-full object-cover rounded-t-md' src={img} alt="image" />
+                <h1 className='mt-4 text-2xl text-primaryColor px-4'>{title}</h1>
+                <p className='text-sm px-4'>{subTitle}</p>
             </div>
-            <div className='p-4 '>
-                <button className='bg-primaryColor w-full py-2 mt-auto rounded-md text-white'>{btn}</button>
-
+            <div className='px-4 '>
+                <button className='bg-primaryColor w-full py-2 rounded-md text-white'>{btn}</button>
             </div>
-
         </div>
     );
 };
 
 export default CardPrimary;
-
